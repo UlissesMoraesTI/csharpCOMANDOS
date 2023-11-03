383bd55eb2a72b2199e6fc25152d202a35027f81
Ulisses Moraes <ulissesmoraes74@gmail.com>
2023-11-02 20:39:48 -0400

    Comandos C# DataTime

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Data.Common;
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+
+dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual);
+
+dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
+
+dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual.ToString("HH:mm:ss"));
+
+dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual.ToString("HH:mm"));
